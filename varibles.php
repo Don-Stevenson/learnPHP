4cffed7382487fd9956b092fc146fbced926d5d4
Don-Stevenson <dsteven1@gmail.com>
2020-03-11 22:29:10 -0400

    add math

--- varibles.php
+++ varibles.php
@@ -16,9 +16,30 @@ $address = array('street' => '123 main street', 'city' => 'Pittsburg')
     <p> Name: <?php echo $f_name . " ".$l_name; ?></p>
      <form action="variables.php" method="get">
         <label> Your Province: </label>
-        <input type="text" name="state"/><br>
-        <label> Your Province: </label>
-        <input type="text" name="state"/><br>
+        <input type="text" name="province"/><br>
+        <label> Number 1: </label>
+        <input type="text" name="num-1"/><br>
+        <label> Number 2: </label>
+        <input type="text" name="num-2"/><br>
+        <input type="submit" value="Submit"/><br>
      </form>
+     <?php 
+     if(isset($_GET) && array_key_exists('province', $_GET)){
+         $state= $_GET['province'];
+         if(isset($state) && !empty($state)){
+            echo "You live in" . $province . '<br>';
+            echo "$f_name lives in $province";
+         }
+            if(count($_GET) >= 3) {
+                $num_1 = $GET['num-1'];
+                $num_2 = $GET['num-2'];
+                echo "$num_1 + $num_2 = " . ($num_1 + $num_2) . "<br>";
+                echo "$num_1 - $num_2 = " . ($num_1 - $num_2) . "<br>";
+                echo "$num_1 * $num_2 = " . ($num_1 * $num_2) . "<br>";
+                echo "$num_1 / $num_2 = " . ($num_1 / $num_2) . "<br>";
+                echo "$num_1 % $num_2 = " . ($num_1 % $num_2) . "<br>";
+            }
+     }
+     ?>
     </body>
 </html>
