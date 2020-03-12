64ab96848ff0e02b849c5bee23be44f8484ef22e
Don-Stevenson <dsteven1@gmail.com>
2020-03-11 22:08:26 -0400

    with html

--- varibles.php
+++ varibles.php
@@ -1,7 +1,19 @@
+<?php
 $f_name = "Steve"
 $l_name = "Smith"
 $age = 44
 $height = 1.88
 $can_vote = true;
 $address = array('street' => '123 main street', 'city' => 'Pittsburg')
+?>
+<!DOCTYPE HTML>
+    <html lang="eng">
+        <head>
+            <meta charset="UTF-8">
+            <title>PHP Tutorial </title>
+        </head>
+    <body>
 
+
+    </body>
+</html>
